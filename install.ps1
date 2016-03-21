3cb20956e2629ac449901fe3e7ce134504fc1357
Philip Daniels <Philip.Daniels1971@gmail.com>
2016-03-21 21:10:06 +0000

    Update docs.

---
+++ install.ps1
@@ -0,0 +1,4 @@
+# Copy resultant exes to the bin folder in my home dir, which is on my path.
+
+cp SetVersion\bin\Release\dnv.exe ~\bin -Verbose
+cp UpdateNuGetDeps\bin\Release\updeps.exe ~\bin -Verbose
